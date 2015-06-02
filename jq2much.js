1514e19669f3d47c83ebed1eb38089033e33fced
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-06-03 01:41:28 +0530

    added ssafe check

--- jq2much.js
+++ jq2much.js
@@ -1,7 +1,12 @@
 registerModule(this,'jq2much', function(jq2much, _jq2much_){
 	
 	jQuery.fn.findSafe = function(selector, ignore) {
-		return this.find(selector).not(this.find(ignore).find(selector));
+		var $nested = this.find(ignore);
+		if($nested.length>0){
+			return this.find(selector).not($nested.find(selector));
+		} else{
+			return this.find(selector);
+		}
 	};
 	
 	
