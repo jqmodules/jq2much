dd3443f19e7731e71a872cc1702358c48c864de0
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-08-01 22:28:29 +0530

    jsons updated

--- jq2much.js
+++ jq2much.js
@@ -1,4 +1,6 @@
-registerModule(this,'jq2much', function(jq2much, _jq2much_){
+_define_('jq2much', function(jq2much, _jq2much_){
+	
+	var jQuery = _module_("jQuery");
 	
 	jQuery.fn.findSafe = function(selector, ignore) {
 		var $nested = this.find(ignore);
@@ -12,14 +14,14 @@ registerModule(this,'jq2much', function(jq2much, _jq2much_){
     if ('querySelector' in document){
     	jQuery.getFirst= function(selector,$c) { 
             if($c){
-            	if($c[0]) return jQuery($c[0].querySelector(selector))
+            	if($c[0]) return jQuery($c[0].querySelector(selector));
             	return jQuery();
-            } return jQuery(document.querySelector(selector))
-        }
+            } return jQuery(document.querySelector(selector));
+        };
     } else {
     	jQuery.getFirst= function(selector,$c) {  
             return jQuery(selector,$c).first();
-        }
+        };
     }
 	
 });
