1c15d784cbe6b8bac619691048282ac95201b24d
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-08-01 22:23:53 +0530

    fixes

--- jq2much.js
+++ jq2much.js
@@ -9,5 +9,17 @@ registerModule(this,'jq2much', function(jq2much, _jq2much_){
 		}
 	};
 	
+    if ('querySelector' in document){
+    	jQuery.getFirst= function(selector,$c) { 
+            if($c){
+            	if($c[0]) return jQuery($c[0].querySelector(selector))
+            	return jQuery();
+            } return jQuery(document.querySelector(selector))
+        }
+    } else {
+    	jQuery.getFirst= function(selector,$c) {  
+            return jQuery(selector,$c).first();
+        }
+    }
 	
 });
